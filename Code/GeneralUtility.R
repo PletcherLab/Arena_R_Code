b8d42b5d2b93dad629e723d052e6180c2158c8cb
Scott Pletcher <spletch@umich.edu>
2024-10-09 14:39:56 -0400

    Added draft transitions code.

--- Code/GeneralUtility.R
+++ Code/GeneralUtility.R
@@ -10,6 +10,42 @@ GetCountingRegions<-function(data){
   r
 }
 
+Get.Transitions.Tracker<-function(tracker,range){
+  rd <- Tracker.GetRawData(tracker, range)
+  tmp<-rle(as.character(rd$CountingRegion))
+  tmp2<-data.frame(tmp$lengths,tmp$values)
+  tmp3<-tmp2[tmp2$tmp.values!="None",]
+  
+  count<-0
+  if(nrow(tmp3)>1){
+    for(i in 2:nrow(tmp3)){
+      if(tmp3$tmp.values[i-1]!=tmp3$tmp.values[i]){
+        count<-count+1
+      }
+    }
+  }
+  count
+}
+
+Get.Transitions.Arena<-function(arena,range=c(0,0)){
+  for(i in 1:nrow(arena$Trackers)){
+    tt<-arena$Trackers[i,]
+    t<-Arena.GetTracker(arena,tt)
+    tmps<-Get.Transitions.Tracker(t,range)
+    if(exists("result",inherits = FALSE)==TRUE){
+      result<-rbind(result,tmps)       
+    }
+    else {
+      result<-tmps     
+    }
+  }
+  result<-data.frame(arena$Trackers,result)
+  names(result)<-c(names(arena$Trackers),"Transitions")
+  row.names(result)<-1:nrow(result)
+  result
+}
+
+
 
 ## Type specific functions
 Plot<-function(tracker, ...) UseMethod("Plot",tracker)
